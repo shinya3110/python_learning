06a6b06bf6cc40c6dd6f7954db8d3055faae42a9
shinya3110 <79698517+shinya3110@users.noreply.github.com>
2021-04-27 23:00:32 +0900

    Add files via upload

--- 0427_minimum_spanning_tree.py
+++ 0427_minimum_spanning_tree.py
@@ -3,26 +3,27 @@ if __name__ == "__main__":
     rslt = [1]
     w = []
 
-    # while len(rsle)!=5:
     tmp = 10
     index = 0
-    for l in range(5):
+    n = 0
+    for l in range(4):
         for i in range (len(rslt)):
-            for j in range (0,4):
+            for j in range (0,5):
                 if data[rslt[i]-1][j] >= 0 and tmp >= data[rslt[i]-1][j]:
                     tmp = data[rslt[i]-1][j]
                     index = j
-        data[rslt[i]-1][index] = -1
-        data[index][rslt[i]-1] = -1
+                    n = rslt[i]
+        data[n-1][index] = -1
+        data[index][n-1] = -1
         rslt.append(index+1)
         w.append(tmp)
+        tmp = 10
+        index = 0
+        n = 0
 
-    # print(tmp)
-    # print(index)
-    print(data)
+    # print(data)
     print("木:" + str(rslt))
     ans = sum(w)
-    print('w:' + str(w))
+    # print('w:' + str(w))
     print("総和:" + str(ans))
-    print(rslt)
-    
+    # print(rslt)
